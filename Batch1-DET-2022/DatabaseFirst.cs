e07d19df0e488428ab9adb122d892b31d43dc0e2
rahulraichur <rahul.raichur99@gmail.com>
2022-09-06 08:42:03 +0530

    rahul

--- Batch1-DET-2022/DatabaseFirst.cs
+++ Batch1-DET-2022/DatabaseFirst.cs
@@ -13,16 +13,16 @@
 //    {
 //        public static void Main(String[] args)
 //        {
-//            //GetAllEmpDetails();
-//            //GetEmpDetailsById();
-//            //AddNewEmployee();
-//            //DeleteEmployee();
-//            //UpdateNewEmployee();
-//            //Console.ReadLine();
-//            //StoredProcedureWithoutPara();
-//            //StoredProcedureWithPara();
-//            //RemovingEmployeeDetails();
-//            //RemovingEmployeeDetails();
+//            GetAllEmpDetails();
+//            GetEmpDetailsById();
+//            AddNewEmployee();
+//            DeleteEmployee();
+//            UpdateNewEmployee();
+//            Console.ReadLine();
+//            StoredProcedureWithoutPara();
+//            StoredProcedureWithPara();
+//            RemovingEmployeeDetails();
+//            RemovingEmployeeDetails();
 //            CallStoredProcwithSQLParamater_insert();
 //        }
 //        private static void CallStoredProcwithSQLParamater_insert()
@@ -132,120 +132,120 @@
 
 
 
-//        //private static void RemovingEmployeeDetails()
-//        //{
-//        //    var ctx = new TrainingContext();
-//        //    var Value = 515;
-//        //    int employee = ctx.Database.ExecuteSqlRaw("DeleteEmpsDetailsByEmpno @p0", Value);
-//        //    Console.WriteLine($"No Of Rows Affected {employee}");
-//        //}
+//        private static void RemovingEmployeeDetails()
+//        {
+//            var ctx = new TrainingContext();
+//            var Value = 515;
+//            int employee = ctx.Database.ExecuteSqlRaw("DeleteEmpsDetailsByEmpno @p0", Value);
+//            Console.WriteLine($"No Of Rows Affected {employee}");
+//        }
 
 
-//        //private static void StoredProcedureWithoutPara()
-//        //{
-//        //    var ctx = new TrainingContext();
+//        private static void StoredProcedureWithoutPara()
+//        {
+//            var ctx = new TrainingContext();
 
-//        //    var employees = ctx.Emps.FromSqlRaw($"GetAllEmpsDetails").ToList();
-
-
-//        //    foreach (var e in employees)
-//        //    {
-//        //        Console.WriteLine(e.Ename);
-//        //    }
-//        //}
-//        //private static void StoredProcedureWithPara()
-//        //{
-//        //    try
-//        //    {
-//        //        var ctx = new TrainingContext();
-
-//        //        var employees = ctx.Emps.FromSqlRaw("GetAllEmpsDetailsByEmpNo @p0", 9999).ToList();
+//            var employees = ctx.Emps.FromSqlRaw($"GetAllEmpsDetails").ToList();
 
 
-//        //        foreach (var e in employees)
-//        //        {
-//        //            Console.WriteLine(e.Ename);
-//        //        }
-//        //    }
+//            foreach (var e in employees)
+//            {
+//                Console.WriteLine(e.Ename);
+//            }
+//        }
+//        private static void StoredProcedureWithPara()
+//        {
+//            try
+//            {
+//                var ctx = new TrainingContext();
 
-//        //    catch(Exception ex)
-//        //    {
-//        //        Console.WriteLine(ex.InnerException.Message);
-//        //    }
-//        //}
-
-//        //private static void UpdateNewEmployee()
-//        //{
-//        //    var ctx = new TrainingContext();
-//        //    var employee = ctx.Emps.Where(e => e.Empno == 9999).SingleOrDefault();
-//        //    try
-//        //    {
-//        //        Emp employee1 = new Emp();
-//        //        // employee.Empno = 9999;
-//        //        employee.Ename = "RR";
-//        //        ctx.Update(employee);
-//        //        ctx.SaveChanges();
-//        //        Console.WriteLine("Updated");
-//        //    }
-//        //    catch (Exception ex)
-//        //    {
-//        //        Console.WriteLine(ex.InnerException.Message);
-//        //    }
-//        //}
+//                var employees = ctx.Emps.FromSqlRaw("GetAllEmpsDetailsByEmpNo @p0", 9999).ToList();
 
 
+//                foreach (var e in employees)
+//                {
+//                    Console.WriteLine(e.Ename);
+//                }
+//            }
 
-//        //private static void DeleteEmployee()
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.InnerException.Message);
+//            }
+//        }
 
-//        //{
-//        //    var ctx = new TrainingContext();
-//        //    try
-//        //    {
-//        //        Emp employee = new Emp();
-//        //        employee.Empno = 9999;
-//        //        ctx.Remove(employee);
-//        //        ctx.SaveChanges();
-//        //        Console.WriteLine("Employee deleted");
-//        //    }
-//        //    catch (Exception ex)
-//        //    {
-//        //        Console.WriteLine(ex.InnerException.Message);
-//        //    }
-//        //private static void AddNewEmployee()
-
-//        //{
-//        //    var ctx = new TrainingContext();
-//        //    try
-//        //    {
-//        //        Emp employee = new Emp();
-//        //        employee.Empno = 9999;
-//        //        employee.Ename = "Rahul R";
-//        //        employee.Sal = 100000;
-//        //       employee.Deptno = 40;
-//        //        employee.Job= "Trainee";
-//        //        ctx.Add(employee);
-//        //       ctx.SaveChanges();
-//        //       Console.WriteLine("New Employee Added");
-//        //    }
-//        //   catch (Exception ex)
-//        //  {
-//        //      Console.WriteLine(ex.InnerException.Message);
-//        //   }
+//        private static void UpdateNewEmployee()
+//        {
+//            var ctx = new TrainingContext();
+//            var employee = ctx.Emps.Where(e => e.Empno == 9999).SingleOrDefault();
+//            try
+//            {
+//                Emp employee1 = new Emp();
+//                // employee.Empno = 9999;
+//                employee.Ename = "RR";
+//                ctx.Update(employee);
+//                ctx.SaveChanges();
+//                Console.WriteLine("Updated");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.InnerException.Message);
+//            }
+//        }
 
 
-//    }
+
+//        private static void DeleteEmployee()
+
+//        {
+//            var ctx = new TrainingContext();
+//            try
+//            {
+//                Emp employee = new Emp();
+//                employee.Empno = 9999;
+//                ctx.Remove(employee);
+//                ctx.SaveChanges();
+//                Console.WriteLine("Employee deleted");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.InnerException.Message);
+//            }
+//            private static void AddNewEmployee()
+
+//            {
+//                var ctx = new TrainingContext();
+//                try
+//                {
+//                    Emp employee = new Emp();
+//                    employee.Empno = 9999;
+//                    employee.Ename = "Rahul R";
+//                    employee.Sal = 100000;
+//                    employee.Deptno = 40;
+//                    employee.Job = "Trainee";
+//                    ctx.Add(employee);
+//                    ctx.SaveChanges();
+//                    Console.WriteLine("New Employee Added");
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine(ex.InnerException.Message);
+//                }
 
 
-//    //private static void GetEmpDetailsById()
-//    //{
-//    //    var ctx = new TrainingContext();
-//    //    var employee = ctx.Emps.Where(e => e.Empno == 7698).SingleOrDefault();
-//    //    Console.WriteLine(employee.Ename + " " + employee.Sal + " "+ employee.Job);
+//            }
 
-//    //    //foreach (var employee in emp)
-//    //    //{
-//    //    //    Console.WriteLine(employee.Ename + " " + employee.Sal);
-//    //    //}
-//    //}
-//}
+
+//            private static void GetEmpDetailsById()
+//            {
+//                var ctx = new TrainingContext();
+//                var employee = ctx.Emps.Where(e => e.Empno == 7698).SingleOrDefault();
+//                Console.WriteLine(employee.Ename + " " + employee.Sal + " " + employee.Job);
+
+//                //foreach (var employee in emp)
+//                //{
+//                //    Console.WriteLine(employee.Ename + " " + employee.Sal);
+//                //}
+//            }
+//        }
 
--- Batch1-DET-2022/LearnLINQ.cs
+++ Batch1-DET-2022/LearnLINQ.cs
@@ -14,157 +14,157 @@
 //        {
 
 
-//            //List<int> num = new List<int>();
-//            //num.Add(100);
-//            //num.Add(103);
-//            //num.Add(105);
-//            //num.Add(102);
+//            List<int> num = new List<int>();
+//            num.Add(100);
+//            num.Add(103);
+//            num.Add(105);
+//            num.Add(102);
 
-//            //var numQuery= num.Where(x => x%2==0);
+//            var numQuery = num.Where(x => x % 2 == 0);
 
-//            //num.Add(205);
-//            //num.Add(301);
-//            //num.Add(400);
-//            //foreach (int number in numQuery)
-//            //{
-//            //    Console.WriteLine("{0}", num);
+//            num.Add(205);
+//            num.Add(301);
+//            num.Add(400);
+//            foreach (int number in numQuery)
+//            {
+//                Console.WriteLine("{0}", num);
 
-//            //}
-//            //Console.ReadLine();
-
-
-
-//            //int[] nums = { 109, 22, 108 };
-
-//            //var result = nums.OrderBy(n => n);
-
-//            //Console.WriteLine("Ordered list of numbers:");
-//            //foreach (int num in result)
-//            //   Console.WriteLine(num);
-
-
-//            //char[] characters = { 'R', 'A', 'H', 'U', 'L', 'R' };
-
-//            //var result = characters.Reverse();
-
-//            //Console.WriteLine("Characters in reverse order:");
-//            //foreach (char character in result)
-//            // Console.WriteLine(character);
-
-//            //string[] city = { "Gandhinagar", "Hyderabad", "Surat", "Belgaum", "Bangalore",
-//            //            "Hubli", "Kolkata", "Bhubaneshwar" };
-
-//            ////var result = city.OrderBy(c => c.Length).ThenBy(c => c);
-//            //var result = city.OrderBy(c => c.Length).ThenByDescending(c => c);
-
-//            //Console.WriteLine("Ordered list of cities");
-//            //foreach (string cities in result)
-//            //   Console.WriteLine(cities);
+//            }
+//            Console.ReadLine();
 
 
 
-//            //string[] words = { "Rahul", "Shishir", "Rohan", "Dhanush", "Rutuja", "Suruchi" };
+//            int[] nums = { 109, 22, 108 };
 
-//            //var result = words.Skip(1);
+//            var result = nums.OrderBy(n => n);
 
-//            //Console.WriteLine("After skipping the word list is :");
-//            //foreach (string word in result)
-//            //    Console.WriteLine(word);
-
-
-
-//            //Intersect
-//            //int[] numbers1 = { 9, 8, 7 };
-//            //int[] numbers2 = { 7, 8, 4 };
-
-//            //var result = numbers1.Intersect(numbers2);
-
-//            //Console.WriteLine("Duplicates in the list are :");
-//            //foreach (int number in result)
-//            //    Console.WriteLine(number);
+//            Console.WriteLine("Ordered list of numbers:");
+//            foreach (int num in result)
+//                Console.WriteLine(num);
 
 
-//            //int[] numbers1 = { 1, 2, 3 };
-//            //int[] numbers2 = { 3, 4, 5 };
+//            char[] characters = { 'R', 'A', 'H', 'U', 'L', 'R' };
 
-//            //var result = numbers1.Union(numbers2);
+//            var result = characters.Reverse();
 
-//            //Console.WriteLine("After Duplicates are removed :");
-//            //foreach (int number in result)
-//            //    Console.WriteLine(number);
+//            Console.WriteLine("Characters in reverse order:");
+//            foreach (char character in result)
+//                Console.WriteLine(character);
 
-//            //var numbers = new int[] { 9, 3, 4, 2, 7 };
+//            string[] city = { "Gandhinagar", "Hyderabad", "Surat", "Belgaum", "Bangalore",
+//                        "Hubli", "Kolkata", "Bhubaneshwar" };
 
-//            //   var result = numbers.Aggregate((a, b) => a * b);
+//            //var result = city.OrderBy(c => c.Length).ThenBy(c => c);
+//            var result = city.OrderBy(c => c.Length).ThenByDescending(c => c);
 
-//            //   Console.WriteLine("Aggregated numbers by multiplication:");
-//            //   Console.WriteLine(result);
-
-//            //int[] numbers = { 1,102,13,19,11,32,78 };
-
-//            //var result = numbers.Average();
-
-//            //Console.WriteLine("Average is:");
-//            //Console.WriteLine(result);
-
-
-//            //string[] names = { "Rahul", "Dhanush", "Rohan", "Shishir", "Abhilash","MSD","virat"};
-
-//            //var result = names.Count();
-
-//            //Console.WriteLine("Number of names :");
-//            //Console.WriteLine(result);
-
-
-//            //ASSIGNMENT 1
-//            //string[] names = { "Shishir", "Dhanush", "Rohan" };
-
-
-//            //var result = names.Where(names => names.Contains("o"));
-
-//            //Console.WriteLine("name which contain o :");
-//            //foreach (string name in result)
-//            //    Console.WriteLine(name);
-
-
-//            ////ASSIGNMENT 2 
-//            //{
-//            //    string[] capitals = { "John", "Peter", "Jacob", "Harry", "Jackson" };
-
-//            //    var res = capitals.OrderBy(c => c.Length);
-//            //    var result = capitals.Skip(4);
-
-//            //    Console.WriteLine("Name which has max no of character length:");
-//            //    foreach (string capital in result)
-//            //        Console.WriteLine(capital);
-//            //}
+//            Console.WriteLine("Ordered list of cities");
+//            foreach (string cities in result)
+//                Console.WriteLine(cities);
 
 
 
+//            string[] words = { "Rahul", "Shishir", "Rohan", "Dhanush", "Rutuja", "Suruchi" };
 
-//            //Assignment 4
+//            var result = words.Skip(1);
 
-//            //string[] words = { "blueberry", "blackberry", "abacus" };
-//            //foreach (string item in words)
-//            //    Console.WriteLine(item.Reverse().ToArray());
-
-
-//            ////Assignment 5
-
-//            //string[] subjects = {"C#", "SQL", "LINQ", "Java"};
-
-//            //var subs=
-
-//            //select subjects[1];
-
-//            //Console.WriteLine("subject is :");
-//            //foreach (var num in Nums)
-//            //{
-//            //    Console.WriteLine(subjects);
-//            //}
+//            Console.WriteLine("After skipping the word list is :");
+//            foreach (string word in result)
+//                Console.WriteLine(word);
 
 
-//            //Assignment 3
+
+//            Intersect
+//            int[] numbers1 = { 9, 8, 7 };
+//            int[] numbers2 = { 7, 8, 4 };
+
+//            var result = numbers1.Intersect(numbers2);
+
+//            Console.WriteLine("Duplicates in the list are :");
+//            foreach (int number in result)
+//                Console.WriteLine(number);
+
+
+//            int[] numbers1 = { 1, 2, 3 };
+//            int[] numbers2 = { 3, 4, 5 };
+
+//            var result = numbers1.Union(numbers2);
+
+//            Console.WriteLine("After Duplicates are removed :");
+//            foreach (int number in result)
+//                Console.WriteLine(number);
+
+//            var numbers = new int[] { 9, 3, 4, 2, 7 };
+
+//            var result = numbers.Aggregate((a, b) => a * b);
+
+//            Console.WriteLine("Aggregated numbers by multiplication:");
+//            Console.WriteLine(result);
+
+//            int[] numbers = { 1, 102, 13, 19, 11, 32, 78 };
+
+//            var result = numbers.Average();
+
+//            Console.WriteLine("Average is:");
+//            Console.WriteLine(result);
+
+
+//            string[] names = { "Rahul", "Dhanush", "Rohan", "Shishir", "Abhilash", "MSD", "virat" };
+
+//            var result = names.Count();
+
+//            Console.WriteLine("Number of names :");
+//            Console.WriteLine(result);
+
+
+//            ASSIGNMENT 1
+//            string[] names = { "Shishir", "Dhanush", "Rohan" };
+
+
+//            var result = names.Where(names => names.Contains("o"));
+
+//            Console.WriteLine("name which contain o :");
+//            foreach (string name in result)
+//                Console.WriteLine(name);
+
+
+//            //ASSIGNMENT 2 
+//            {
+//                string[] capitals = { "John", "Peter", "Jacob", "Harry", "Jackson" };
+
+//                var res = capitals.OrderBy(c => c.Length);
+//                var result = capitals.Skip(4);
+
+//                Console.WriteLine("Name which has max no of character length:");
+//                foreach (string capital in result)
+//                    Console.WriteLine(capital);
+//            }
+
+
+
+
+//            Assignment 4
+
+//            string[] words = { "blueberry", "blackberry", "abacus" };
+//            foreach (string item in words)
+//                Console.WriteLine(item.Reverse().ToArray());
+
+
+//            //Assignment 5
+
+//            string[] subjects = { "C#", "SQL", "LINQ", "Java" };
+
+//            var subs =
+
+//            select subjects[1];
+
+//            Console.WriteLine("subject is :");
+//            foreach (var num in Nums)
+//            {
+//                Console.WriteLine(subjects);
+//            }
+
+
+//            Assignment 3
 
 //            string[] words = { "believe", "relief", "reciept", "field" };
 //            foreach (string m in words)
@@ -172,45 +172,45 @@
 
 
 
-//            //Example
+//            Example
 
-//            //{
-//            //    List<string> words = new List<string> { "Rahul", "Rohan", "Rutuja", "Dhanush", "Dharmendra", "shishir", "suruchi" };
+//{
+//                List<string> words = new List<string> { "Rahul", "Rohan", "Rutuja", "Dhanush", "Dharmendra", "shishir", "suruchi" };
 
-//            //    var WordGroups = words.GroupBy(x => x[0]).Select(y => new { FirstLetter = y.Key, Words = y });
+//                var WordGroups = words.GroupBy(x => x[0]).Select(y => new { FirstLetter = y.Key, Words = y });
 
-//            //    foreach(var item in WordGroups)
-//            //    {
-//            //        Console.WriteLine("words that start with" + "letter '{0}':", item.FirstLetter);
+//                foreach (var item in WordGroups)
+//                {
+//                    Console.WriteLine("words that start with" + "letter '{0}':", item.FirstLetter);
 
-//            //        foreach (var w in item.Words)
-//            //        {
-//            //            Console.WriteLine(w);
-//            //        }
-//            //    }
-//            //}
+//                    foreach (var w in item.Words)
+//                    {
+//                        Console.WriteLine(w);
+//                    }
+//                }
+//            }
 
 
-//            //Example 2
+//            Example 2
 
-//           // List<string> words = new List<string> { "Rahul", "Rohan", "Rutuja", "Dhanush", "Dharmendra", "shishir", "suruchi" };
-            
-//           // foreach (string m in words)
-//           // {
-//           //     Console.WriteLine($"{m} -- {m.Length}");
-//           // }
-//           // var WordGroups = words.GroupBy(x => x.Length).Select(y => new { FirstLetter = y.Key, Words = y });
-//           // foreach (var item in WordGroups)
-//           //{
-//           //       Console.WriteLine("words that start with" + "letter '{0}':", item.FirstLetter);
+// List<string> words = new List<string> { "Rahul", "Rohan", "Rutuja", "Dhanush", "Dharmendra", "shishir", "suruchi" };
 
-//           //            foreach (var w in item.Words)
-//           //            {
-//           //                Console.WriteLine(w);
-//           //            }
-                
+//            foreach (string m in words)
+//            {
+//                Console.WriteLine($"{m} -- {m.Length}");
+//            }
+//            var WordGroups = words.GroupBy(x => x.Length).Select(y => new { FirstLetter = y.Key, Words = y });
+//            foreach (var item in WordGroups)
+//            {
+//                Console.WriteLine("words that start with" + "letter '{0}':", item.FirstLetter);
 
-//           // }
+//                foreach (var w in item.Words)
+//                {
+//                    Console.WriteLine(w);
+//                }
+
+
+//            }
 
 
 
